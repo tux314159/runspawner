5ff87e238dddf3859d5188644edb0b0e2d5a186a
Isaac <tux314159@gmail.com>
2023-01-31 16:17:18 +0800

    Reorganise shit

---
+++ app/toy/Main.hs
@@ -0,0 +1,24 @@
+module Main (main) where
+
+import Container
+
+main :: IO ()
+main =
+  withContainer
+    (ContainerBase "/home/isaac/containers/alpine")
+    (\contDo -> do
+      contDo CCPutStr "echo hi\n"
+      contDo CCWaitShCmd
+      putStrLn =<< contDo CCGetLine
+      contDo CCPutStr "echo bye\n"
+      contDo CCWaitShCmd
+      putStrLn =<< contDo CCGetLine
+      contDo CCPutStr "ls\n"
+      contDo CCWaitShCmd
+      putStr =<< contDo CCGetAll
+
+      --getLine >>= contDo CCPutStr
+      --contDo CCWaitShCmd
+      --putStr =<< contDo CCGetAll
+      return ()
+    )
